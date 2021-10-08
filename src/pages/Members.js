5e2a86177f0aef192b0cd8c7396b18c0248e9d26
Louis <87426228+louissalerno@users.noreply.github.com>
2021-10-08 22:44:07 +0100

    Update Members.js

---
+++ src/pages/Members.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const members = () => {
+    return (
+        <div>
+            <Navigation/>
+        </div>
+    )
+}
+
+export default members
