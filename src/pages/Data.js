d569848aa15f12a94dd423ee88847767893e5f08
Louis <87426228+louissalerno@users.noreply.github.com>
2021-10-09 21:58:41 +0100

    Update Data.js

---
+++ src/pages/Data.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../components/Header";
+// import Mainpart from "../components/Mainpart";
+const Data = () => {
+    return (
+        <div>
+            <Header />
+            {/* <Mainpart /> */}
+        </div>
+    )
+}
+
+export default Data
